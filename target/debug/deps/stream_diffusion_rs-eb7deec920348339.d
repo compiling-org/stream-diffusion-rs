1a71757a42160b9b7aebcd4eab3ce81e7e6e6226
Sleepless Monk <kapil.bambardekar@gmail.com>
2025-10-23 15:05:58 +0530

    build: generate build artifacts

---
+++ target/debug/deps/stream_diffusion_rs-eb7deec920348339.d
@@ -0,0 +1,5 @@
+C:\Users\kapil\compiling\stream-diffusion-rust\target\debug\deps\stream_diffusion_rs-eb7deec920348339.d: src\lib.rs
+
+C:\Users\kapil\compiling\stream-diffusion-rust\target\debug\deps\libstream_diffusion_rs-eb7deec920348339.rmeta: src\lib.rs
+
+src\lib.rs:
